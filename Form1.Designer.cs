e8fd12e783b1f1f0922ae43f35645b2aa98357ab
Diego Biasatti <biasatti.diego@gmail.com>
2022-04-26 14:46:23 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace killer_clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_hora = new System.Windows.Forms.Label();
+            this.horario = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btn_aceptar = new System.Windows.Forms.Button();
+            this.lbl_hora_cerrar = new System.Windows.Forms.Label();
+            this.txt_file_path = new System.Windows.Forms.TextBox();
+            this.btn_file = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.enabled = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lbl_hora
+            // 
+            this.lbl_hora.AutoSize = true;
+            this.lbl_hora.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_hora.Location = new System.Drawing.Point(14, 44);
+            this.lbl_hora.Name = "lbl_hora";
+            this.lbl_hora.Size = new System.Drawing.Size(68, 20);
+            this.lbl_hora.TabIndex = 0;
+            this.lbl_hora.Text = "Horario";
+            // 
+            // horario
+            // 
+            this.horario.Enabled = true;
+            this.horario.Interval = 1000;
+            this.horario.Tick += new System.EventHandler(this.horario_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 80);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(120, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btn_aceptar
+            // 
+            this.btn_aceptar.Location = new System.Drawing.Point(143, 78);
+            this.btn_aceptar.Name = "btn_aceptar";
+            this.btn_aceptar.Size = new System.Drawing.Size(119, 23);
+            this.btn_aceptar.TabIndex = 2;
+            this.btn_aceptar.Text = "A C E P T A R";
+            this.btn_aceptar.UseVisualStyleBackColor = true;
+            this.btn_aceptar.Click += new System.EventHandler(this.btn_aceptar_Click);
+            // 
+            // lbl_hora_cerrar
+            // 
+            this.lbl_hora_cerrar.AutoSize = true;
+            this.lbl_hora_cerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_hora_cerrar.Location = new System.Drawing.Point(138, 44);
+            this.lbl_hora_cerrar.Name = "lbl_hora_cerrar";
+            this.lbl_hora_cerrar.Size = new System.Drawing.Size(68, 20);
+            this.lbl_hora_cerrar.TabIndex = 3;
+            this.lbl_hora_cerrar.Text = "Horario";
+            // 
+            // txt_file_path
+            // 
+            this.txt_file_path.Location = new System.Drawing.Point(12, 114);
+            this.txt_file_path.Name = "txt_file_path";
+            this.txt_file_path.Size = new System.Drawing.Size(120, 20);
+            this.txt_file_path.TabIndex = 4;
+            // 
+            // btn_file
+            // 
+            this.btn_file.Location = new System.Drawing.Point(143, 112);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(119, 23);
+            this.btn_file.TabIndex = 5;
+            this.btn_file.Text = "F I L E";
+            this.btn_file.UseVisualStyleBackColor = true;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hora Actual";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(134, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hora de Corte";
+            // 
+            // enabled
+            // 
+            this.enabled.AutoSize = true;
+            this.enabled.Location = new System.Drawing.Point(2, 2);
+            this.enabled.Name = "enabled";
+            this.enabled.Size = new System.Drawing.Size(15, 14);
+            this.enabled.TabIndex = 8;
+            this.enabled.UseVisualStyleBackColor = true;
+            this.enabled.CheckedChanged += new System.EventHandler(this.enabled_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 151);
+            this.Controls.Add(this.enabled);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_file);
+            this.Controls.Add(this.txt_file_path);
+            this.Controls.Add(this.lbl_hora_cerrar);
+            this.Controls.Add(this.btn_aceptar);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lbl_hora);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kill Process por Horario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_hora;
+        private System.Windows.Forms.Timer horario;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btn_aceptar;
+        private System.Windows.Forms.Label lbl_hora_cerrar;
+        private System.Windows.Forms.TextBox txt_file_path;
+        private System.Windows.Forms.Button btn_file;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox enabled;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace killer_clock
+{
+    public partial class Form1 : Form
+    {
+        DateTime t1, t2;
+        string hora;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //estas lineas estan agregadas para que arranque, ya seteado para Kill MP
+            enabled.Checked = true;
+            hora = "00:01:00";
+            lbl_hora_cerrar.Text = hora;
+            t1 = DateTime.Parse(hora);
+            txt_file_path.Text = "Mercadopago";
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            t1 = DateTime.Parse(dateTimePicker1.Text);
+        }
+
+        private void btn_aceptar_Click(object sender, EventArgs e)
+        {
+            lbl_hora_cerrar.Text = t1.ToString("HH:mm:ss tt");
+        }
+
+        private void horario_Tick(object sender, EventArgs e)
+        {
+            hora = DateTime.Now.ToString("HH:mm:ss tt");
+            
+            lbl_hora.Text = hora;
+            
+            t2 = DateTime.Parse(hora);
+            
+            if(TimeSpan.Compare(t1.TimeOfDay, t2.TimeOfDay) == 0)
+            {
+                kill(txt_file_path.Text);
+            }
+        }
+
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) 
+            {
+                txt_file_path.Text = openFileDialog1.SafeFileName.Replace(".exe","");
+            }
+        }
+
+        private void enabled_CheckedChanged(object sender, EventArgs e)
+        {
+            if (enabled.Checked)
+            {
+                btn_aceptar.Enabled = false;
+                btn_file.Enabled = false;
+                txt_file_path.Enabled = false;
+                dateTimePicker1.Enabled = false;
+            }
+            else
+            {
+                btn_aceptar.Enabled = true;
+                btn_file.Enabled = true;
+                txt_file_path.Enabled = true;
+                dateTimePicker1.Enabled =true;
+            }
+        }
+
+        private void kill(string proceso)
+        { int i;
+            Process[] runingProcess = Process.GetProcesses();
+            for (i = 0; i < runingProcess.Length; i++)
+            {
+               
+                if (runingProcess[i].ProcessName == proceso)
+                {
+                    runingProcess[i].Kill();
+                }
+
+            }
+        }
+
+    }
+}
